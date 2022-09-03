dfc2d7a0d90d0dde99ac819d93a7b970650146cb
Sasha-Villegas <sashavillegas30@gmail.com>
2022-09-03 01:24:56 -0300

    Contador

--- src/components/mockData/mockData.js
+++ src/components/mockData/mockData.js
@@ -1,30 +1,30 @@
 const data = [
     {
-        "id": 1, "tittle": "Body Kit", precio: "$10000", img: '../../../images/body-kit.jpg'
+        "id": 1, "tittle": "Body Kit", "precio" : "$10000", "img": "../images/body-kit.jpg"
     },
     {
-        "id": 2, "tittle": "Hair Kit", precio: "$8000", img: '../../../images/kit-capilar.jpg',   
+        "id": 2, "tittle": "Hair Kit", "precio" : "$8000", "img" :"../images/kit-capilar.jpg",   
     },
     {
-        "id": 3, "tittle": "Leg Kit", precio: "$7000", img: '../../../images/kit-piernas.jpg', 
+        "id": 3, "tittle": "Leg Kit", "precio" : "$7000", "img" : "../images/kit-piernas.jpg", 
     },
     {
-        "id": 4, "tittle": "Serum", precio: "$3500", img: '../../../images/serum-revitalizador.jpg',   
+        "id": 4, "tittle": "Serum", "precio" : "$3500", "img" : "../images/serum-revitalizador.jpg",   
     },
     {
-        "id": 5, "tittle": "Aromatic Soap", precio: "$1500", img: '../../../images/jabones-aromaticos.jpg', 
+        "id": 5, "tittle": "Aromatic Soap", "precio" : "$1500", "img" :"../images/jabones-aromaticos.jpg", 
     },
     {
-        "id": 6, "tittle": "Revitalizing Oil", precio: "$2600", img:  '../../../images/aceite-revitalizador.jpg',
+        "id": 6, "tittle": "Revitalizing Oil", "precio" : "$2600", "img" :  "../images/aceite-revitalizador.jpg",
     },
     {
-        "id": 7, "tittle": "Facial Cream", precio: "$3400", img: '../../../images/crema-facial.jpg',
+        "id": 7, "tittle": "Facial Cream", "precio" : "$3400", "img" : "../images/crema-facial.jpg",
     },
     {
-        "id": 8, "tittle": "Aromatic Candles", precio: "$1800", img: 'images/velas-aromaticos.jpg',
+        "id": 8, "tittle": "Aromatic Candles", "precio" : "$1800", "img" : "../../images/velas-aromaticos.jpg",
     },
     {
-        "id": 9, "tittle": "Organic Mask", precio: "$4500", img: '../../../images/mascarilla-organica.jpg',
+        "id": 9, "tittle": "Organic Mask", "precio" : "$4500", "img" : "../../images/mascarilla-organica.jpg",
     }
 ]
 
